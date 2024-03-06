30bd92988e111b9776e59512a6c87d3e21d38f8e
Sabreena Daniyal <160657530+sabreenadaniyal@users.noreply.github.com>
2024-03-07 02:45:58 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+"use strict";
+//Addition
+let num1 = 5;
+let num2 = 2;
+console.log(num1 + num2);
+//Subtraction
+let num3 = 20;
+let num4 = 15;
+console.log(num3 - num4);
+//Multiplication
+let num5 = 6;
+let num6 = 3;
+console.log(num5 * num6);
+//Division
+let num7 = 6;
+let num8 = 3;
+console.log(num7 / num8);
+//Modulus
+let num9 = 8;
+let num10 = 4;
+console.log(num9 % num10);
+//Exponent
+let num11 = 2;
+let num12 = 4;
+console.log(2 ** 4);
---
+++ main.ts
@@ -0,0 +1,29 @@
+//Addition
+let num1:number=5
+let num2:number=2
+console.log(num1+num2);
+
+//Subtraction
+let num3:number=20
+let num4:number=15
+console.log(num3-num4);
+
+//Multiplication
+let num5:number=6
+let num6:number=3
+console.log(num5*num6);
+
+//Division
+let num7:number=6
+let num8:number=3
+console.log(num7/num8);
+
+//Modulus
+let num9:number=8
+let num10:number=4
+console.log(num9%num10);
+
+//Exponent
+let num11:number=2
+let num12:number=4
+console.log(2**4);
